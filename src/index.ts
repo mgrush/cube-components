ac95bec5145f088847eb8e2269fa601a8b8c75d6
MacBook <macbook@MacBookdeMacBook-Pro.local>
2019-08-23 13:48:13 +0800

    修复导出对象

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ export { default as Button } from './Button'
 export { default as Icon } from './Icon'
 export { default as Layer } from './Layer'
 
-export { withTheme, ThemeContext } from './styled/withTheme'
+export { withTheme, ThemeContext } from './styled'
